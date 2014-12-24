41dc17607f89e0d01a020db411851d3447cfdaa0
Joshua <joshuabg@verizon.net>
2014-12-24 16:58:32 -0500

    Remove commented lines
    It's git, can always revert.

--- floundercommits.sh
+++ floundercommits.sh
@@ -17,12 +17,3 @@ cd external/iproute2
 git fetch https://github.com/joshuabg/android_external_iproute2.git
 git cherry-pick 478ce3e5c57e11433b64b1dafdb8ea437fd69143
 cd ../..
-
-# umm
-#cd external/ffmpeg
-#git fetch http://review.cyanogenmod.org/CyanogenMod/android_external_ffmpeg refs/changes/35/82835/2 && git cherry-pick FETCH_HEAD
-#git fetch https://github.com/joshuabg/android_external_ffmpeg.git
-#git cherry-pick a8edb2d0fef655deec90bc9c9e51b738973a20e7
-#git cherry-pick f3af1e5b87e1d6908096ad54c03cd90f3ceea40f
-#cd ../..
-
