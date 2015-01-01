5ba38125b1b8639c29256f31e25cbc62b293c1c5
Joshua <joshuabg@verizon.net>
2015-01-01 09:39:18 -0500

    Remove cherry-pick for iproute2
    CM merged it

--- floundercommits.sh
+++ floundercommits.sh
@@ -8,9 +8,3 @@ cd bionic
 git fetch https://github.com/DirtyUnicorns/android_bionic.git
 git cherry-pick 1cbfd7abee7cbbf0132ee8ee6190bd6e2f17ee21
 cd ..
-
-# signal.h
-cd external/iproute2
-git fetch https://github.com/joshuabg/android_external_iproute2.git
-git cherry-pick 478ce3e5c57e11433b64b1dafdb8ea437fd69143
-cd ../..
