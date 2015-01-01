44051e1f375bee0e8db54a76b3877426622a4992
Joshua <joshuabg@users.noreply.github.com>
2015-01-01 17:21:54 -0500

    Remove cherry-pick for frameworks_base
    Merged on CM and AICP

--- floundercommits.sh
+++ floundercommits.sh
@@ -8,7 +8,3 @@ cd bionic
 git fetch https://github.com/DirtyUnicorns/android_bionic.git
 git cherry-pick 1cbfd7abee7cbbf0132ee8ee6190bd6e2f17ee21
 cd ..
-cd frameworks/base
-git fetch https://github.com/joshuabg/frameworks_base.git
-git cherry-pick 1a5f913740e60115f506b44bdc43ee5c139a6dce
-cd ../..
